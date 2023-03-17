1b48ac1afaf81fc9b951e0df37b74fb9eee194ed
spark3i <djubreel09@gmail.com>
2023-03-17 10:04:40 -0700

    coding with c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main is the starting point
+ *return when it is zero #(succesful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"); 
+	return (0);
+}
