89f51670e8890437caee7d2a554f7499f4eb7c91
spark3i <djubreel09@gmail.com>
2023-03-17 11:03:06 -0700

    what a language

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main - beginning
+  *return -0 #(succesful)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
