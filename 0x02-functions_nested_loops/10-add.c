8a8f5738039ac3180f0f373645606c0ca549dc06
spark3i <djubreel09@gmail.com>
2023-03-22 14:24:49 -0700

    addittion

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *add - adds two numbers and give the results
+  *@a: int to be added
+  *@b: int to be added
+  *Return: sum of a and b
+  */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,16 @@ int print_last_digit(int n)
 {
 	int a;
 
-	a = n % 10;
-	while (n > 0)
+	if (n < 0)
 	{
-		return (a);
+		n = -n;
 	}
-	return (0);
+
+	a = n % 10;
+	if (a > 0)
+	{
+		a = -a;
+	_putchar(a + '0');
+	}
+	return (a);
 }
