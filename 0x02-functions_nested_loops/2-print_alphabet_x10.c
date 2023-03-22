14a26a516ada038b36bd60ee06b60b051fcd9232
spark3i <djubreel09@gmail.com>
2023-03-22 12:39:48 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  *main- is entry point
+  *Description: code to print the lowercase letters x10
+  *return auccess at 0
+  */
+void print_alphabet_x10(void)
+{
+	
+	int i = 0;
+
+	while (i < 10)
+	{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+	i++;
+	}
+}
