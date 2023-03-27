a2ad0d88118c81217e4c0410e92252e14ec7b044
spark3i <djubreel09@gmail.com>
2023-03-27 10:16:47 -0700

    put half

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,4 @@
-#include "mai.h"
+#include "main.h"
 /**
   *print_rev- prints string in reverse
   *@s: character to print in reverse
@@ -6,5 +6,19 @@
 void print_rev(char *s)
 {
 	int letter;
+	int o;
+
 	while (*s != '\0')
+	{
+		letter++;
+		s++;
+	}
+	s--;
+	for (o = letter; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
 
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  *puts2- prints half of the string
+  *@str: strings to print
+  */
+void puts2(char *str)
+{
+	int len = 0;
+	int m = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	m = len -1;
+	for (o = 0; o <= m; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+	}
+	_putchar('\n');
+}
+
