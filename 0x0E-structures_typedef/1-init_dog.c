d4a5314278a9453f559b892f36ffc28519c93f4a
spark3i <djubreel09@gmail.com>
2023-04-17 05:59:40 -0700

    allocating memory

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 /**
   *init_dog- initailize variable type dog
   *@d: variable name
@@ -9,7 +10,9 @@
   */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d ->name = name;
-	d ->age = age;
-	d ->owner = owner;
+	if (d == NULL)
+		d = malloc(sizeof(struct dog);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
